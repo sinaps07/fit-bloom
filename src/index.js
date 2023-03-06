ccd34359e28e20faa0af7885e3dfcdcaeba8891b
sujit kumar <sujitkymar101@gmail.com>
2023-03-07 01:10:48 +0530

    components folder created

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Heading from './Heading';
+import Heading from './components/Heading';
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
