64d43114442acfa8c3e074c9742a82da59f1734c
sujit kumar <sujitkymar101@gmail.com>
2023-03-07 00:53:27 +0530

    landing page added

---
+++ src/Heading.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+
+const Heading=()=>{
+    return (
+    <>
+        <div>
+        <h3>make your body fit this spring with us</h3>
+
+        <h1>improve your fitness level for the better </h1>
+        
+        <button>train me</button>
+
+
+        </div>
+
+     </>
+
+
+    )
+}
+export default Heading;
