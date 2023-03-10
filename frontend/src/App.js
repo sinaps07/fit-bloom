65d3f5c1960fae1fbfd8e3bcf380ec7ec0be2de2
aditi10701 <aditiprasad4@gmail.com>
2023-03-10 12:03:44 +0530

    navbar and footer added

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/navbar';
+import HomePage from './components/homePage/HomePage';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <HomePage/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
